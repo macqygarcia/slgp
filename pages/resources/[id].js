e3cd8440632fe87cbc738020609010bda991f5ca
macqygarcia <garciamacqy.dilg@gmail.com>
2022-04-04 23:19:26 +0800

    Ignore commit

--- pages/resources/[id].js
+++ pages/resources/[id].js
@@ -1,30 +1,30 @@
 import { useRouter } from 'next/router'
 
 export default function Resource({ resource }) {
+  const router = useRouter()
   if (!resource) {
     return null
   }
-  const router = useRouter()
   const { id } = router.query
   return <div>{id}</div>
 }
 
-export async function getStaticPaths() {
-  // Get all the list of pokemon
-  const resp = await fetch(
-    'https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json'
-  )
+// export async function getStaticPaths() {
+//   // Get all the list of pokemon
+//   const resp = await fetch(
+//     'https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json'
+//   )
 
-  const pokemon = await resp.json()
+//   const pokemon = await resp.json()
 
-  return {
-    // paths: [{ params: { id: "1" }}]
-    paths: pokemon.map(pokemon => ({
-      params: { id: pokemon.id.toString() }
-    })),
-    fallback: false
-  }
-}
+//   return {
+//     // paths: [{ params: { id: "1" }}]
+//     paths: pokemon.map(pokemon => ({
+//       params: { id: pokemon.id.toString() }
+//     })),
+//     fallback: false
+//   }
+// }
 
 // export async function getStaticProps({ params }) {
 //   const resp = await fetch(
